591965f6679eff6b8cc1a897d3bbf662e88bb56e
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-03-07 10:46:59 -0800

    name passes extra to child act
    to-do:
     - toggle colors on click

--- app/src/main/java/com/example/shymacbook/picturematch/game.java
+++ app/src/main/java/com/example/shymacbook/picturematch/game.java
@@ -1,13 +1,24 @@
 package com.example.shymacbook.picturematch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class game extends AppCompatActivity {
+
+    private TextView showName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        showName = (TextView) findViewById(R.id.nameSpot);
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+            String textEntered = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+            showName.setText(textEntered);
+        }
     }
 }

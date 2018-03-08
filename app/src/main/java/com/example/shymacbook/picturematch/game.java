cb02190623fde5d3207fb6186551fcb44ebc93ea
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-03-08 15:31:16 -0800

    activity 2 initialization fixed
    i had the initialization in the wrong spot for game.java . should have defined vars in the onCreate method...

--- app/src/main/java/com/example/shymacbook/picturematch/game.java
+++ app/src/main/java/com/example/shymacbook/picturematch/game.java
@@ -9,17 +9,30 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class game extends AppCompatActivity {
 
     private TextView showName;
-    int clickCount;
-    ImageView card_1;
-    int card_1_ID;
-    ImageView card_2;
-    int card_2_ID;
+    private int clickCount;
+    private ImageView card_first;
+    private ImageView card_second;
+    private ImageView card_0;
+    private ImageView card_1;
+    private ImageView card_2;
+    private ImageView card_3;
+    private ImageView card_4;
+    private ImageView card_5;
+    private int card_0_ID;
+    private int card_1_ID;
+    private int card_2_ID;
+    private int card_3_ID;
+    private int card_4_ID;
+    private int card_5_ID;
+    private Integer[] flipArray = {0,0,0,0,0,0};
+    private String[] colorArray;
+    private ImageView[] cardArray;
 
 
     @Override
@@ -27,6 +40,32 @@ public class game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         clickCount = 0;
+
+        card_0 = (ImageView)findViewById(R.id.card0);
+        card_1 = (ImageView)findViewById(R.id.card1);
+        card_2 = (ImageView)findViewById(R.id.card2);
+        card_3 = (ImageView)findViewById(R.id.card3);
+        card_4 = (ImageView)findViewById(R.id.card4);
+        card_5 = (ImageView)findViewById(R.id.card5);
+
+
+        ImageView[] cardArray = {card_0, card_1,  card_2, card_3, card_4, card_5};
+        String[] colorArray = {"red", "blue", "yellow", "red", "blue", "yellow"};
+        Integer[] flipArray = {0,0,0,0,0,0};
+
+        cardArray[0].setBackgroundResource(R.color.red);
+        cardArray[1].setBackgroundResource(R.color.blue);
+        cardArray[2].setBackgroundResource(R.color.yellow);
+        cardArray[3].setBackgroundResource(R.color.red);
+        cardArray[4].setBackgroundResource(R.color.blue);
+        cardArray[5].setBackgroundResource(R.color.yellow);
+
+        cardArray[0].setVisibility(View.INVISIBLE);
+        cardArray[1].setVisibility(View.INVISIBLE);
+        cardArray[2].setVisibility(View.INVISIBLE);
+        cardArray[3].setVisibility(View.INVISIBLE);
+        cardArray[4].setVisibility(View.INVISIBLE);
+        cardArray[5].setVisibility(View.INVISIBLE);
 
         showName = (TextView) findViewById(R.id.nameSpot);
         Intent intentThatStartedThisActivity = getIntent();
@@ -38,41 +77,35 @@ public class game extends AppCompatActivity {
 
     public void flip(View view) {
         clickCount++;
+        Log.d("card clicks", "card click counter: " + clickCount);
         if (clickCount == 1){
-            // assign card 1
-            card_1_ID = view.getId();
-            card_1 = (ImageView)findViewById(card_1_ID);
-            Log.d("card clicks", "card 1 assigned");
-            Log.d("card clicks", "card click counter: " + clickCount);
-            // flip image
-            card_1.setBackgroundResource(R.color.red);
+            card_first = (ImageView)findViewById(view.getId());
+            Log.d("card clicks", "card_first assigned");
+//            card_first.setImageAlpha(1);
+            card_first.setVisibility(View.VISIBLE);
         }
         if (clickCount == 2){
-            // do comparison here, now that 2 cards are selected
-            // assign card 2
-            card_2_ID = view.getId();
-            card_2 = (ImageView)findViewById(card_2_ID);
-            Log.d("card clicks", "card 2 assigned");
-            Log.d("card clicks", "card click counter: " + clickCount);
-            // flip image
-            
-            // compare stuff
+            card_second = (ImageView)findViewById(view.getId());
+            Log.d("card clicks", "card_second assigned");
+//            card_first.setImageAlpha(1);
+            card_first.setVisibility(View.VISIBLE);
+            if (card_first.getBackground().equals(card_second.getBackground())){
+                clickCount = 0;
+                Log.d("card clicks reset", "card click counter: " + clickCount);
+                Toast.makeText(this, "MATCH!", Toast.LENGTH_SHORT).show();
+            }
+            else if ( !(card_first.getBackground().equals(card_second.getBackground())) ){
+                Toast.makeText(this, "wrong...", Toast.LENGTH_SHORT).show();
+                clickCount = 0;
+                Log.d("card clicks reset", "card click counter: " + clickCount);
+//                card_first.setImageAlpha(0);
+//                card_second.setImageAlpha(0);
+                card_first.setVisibility(View.INVISIBLE);
+                card_second.setVisibility(View.INVISIBLE);
 
-            // if they match, reward! and reset the clicks to 0
-
-        }
-        if (clickCount == 3){
-            Log.d("card clicks", "card click counter: " + clickCount);
-            // not a match, reset the cards and counter
-
-
-
-            clickCount = 0;
-            Log.d("card clicks", "card click counter reset to " + clickCount);
+            }
         }
     }
-
-
 
 
 
